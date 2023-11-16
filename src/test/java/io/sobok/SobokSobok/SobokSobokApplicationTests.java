588d6d8a54185caed89a4cc62da921e960340b0b
dev-Crayon <seungheon328@gmail.com>
2023-11-16 14:00:29 +0900

    [Init]: 소복소복 Spring 프로젝트 생성

---
+++ src/test/java/io/sobok/SobokSobok/SobokSobokApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sobok.SobokSobok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SobokSobokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
