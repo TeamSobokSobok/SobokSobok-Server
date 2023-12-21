2fd9365969838def9f105daee35341a9f1c26f76
Lee SeungHeon <51286325+dev-Crayon@users.noreply.github.com>
2023-12-21 23:04:29 +0900

    로그아웃 API 구현 (#16)
    * [Feat]: 로그아웃 API 구현
    
    ErrorCode 추가
    SuccessCode 추가
    
    Related to: #15
    
    * [Feat]: 로그아웃 API 구현
    
    Spring Security 권한 문제 수정
    
    Related to: #15
    
    * [Feat]: 로그아웃 API 구현
    
    로그아웃 관련 코드 구현
    
    Related to: #15

--- src/main/java/io/sobok/SobokSobok/config/SecurityConfig.java
+++ src/main/java/io/sobok/SobokSobok/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package io.sobok.SobokSobok.config;
 import io.sobok.SobokSobok.security.jwt.JwtCustomFilter;
 import io.sobok.SobokSobok.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,13 +30,15 @@ public class SecurityConfig {
                 .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-//                        .requestMatchers(PathRequest.toH2Console()).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/v3/**")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/actuator/health")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/profile")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
-                                .anyRequest().authenticated()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/v3/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/actuator/health")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/profile")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/auth/signup")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/auth/login")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/auth/refresh")).permitAll()
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(
                         new JwtCustomFilter(jwtProvider),
