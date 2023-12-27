c69abd71ecedbc2c47309356fb06841b101aeba9
dev-Crayon <seungheon328@gmail.com>
2023-12-27 17:50:24 +0900

    [Feat]: 약 추가 API 구현
    Querydsl 세팅
    
    Related to: #26

---
+++ src/main/java/io/sobok/SobokSobok/config/QuerydslConfig.java
@@ -0,0 +1,19 @@
+package io.sobok.SobokSobok.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
