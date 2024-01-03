10e4aac4d6706e0a3585e1ccf6ebb258caada89d
dev-Crayon <seungheon328@gmail.com>
2024-01-03 21:54:54 +0900

    [Setting]: Notice Entity 객체 정의
    Notice.java 구현
    NoticeStatus.java
    NoticeType.java
    
    Related to: #35

---
+++ src/main/java/io/sobok/SobokSobok/notice/domain/NoticeType.java
@@ -0,0 +1,14 @@
+package io.sobok.SobokSobok.notice.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum NoticeType {
+
+    PILL,
+    FRIEND,
+    ;
+
+}
