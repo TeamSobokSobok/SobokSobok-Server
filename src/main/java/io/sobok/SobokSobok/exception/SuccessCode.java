48370796c8d6c8c274fa6dba70f563f3b2812a5d
Lee SeungHeon <51286325+dev-Crayon@users.noreply.github.com>
2024-01-13 10:24:35 +0900

    [Feat]: 알림 약 상세조회 API 구현 (#47)
    관련 파일 생성
    로직 작성
    
    Related to: #46

--- src/main/java/io/sobok/SobokSobok/exception/SuccessCode.java
+++ src/main/java/io/sobok/SobokSobok/exception/SuccessCode.java
@@ -28,6 +28,7 @@ public enum SuccessCode {
 
     // notice
     GET_NOTICE_LIST_SUCCESS(HttpStatus.OK, "알림 리스트 조회에 성공했습니다."),
+    GET_RECEIVE_PILL_INFO_SUCCESS(HttpStatus.OK, "전달받은 약 정보 조회에 성공했습니다."),
     ;
 
     private final HttpStatus code;
