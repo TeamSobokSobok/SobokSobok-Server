8db92e4bca000502d6b74e6a21f542805b0d1e3a
dev-Crayon <seungheon328@gmail.com>
2024-01-13 10:21:58 +0900

    [Feat]: 알림 약 상세조회 API 구현
    관련 파일 생성
    로직 작성
    
    Related to: #46

--- src/main/java/io/sobok/SobokSobok/notice/infrastructure/NoticeQueryRepository.java
+++ src/main/java/io/sobok/SobokSobok/notice/infrastructure/NoticeQueryRepository.java
@@ -7,12 +7,15 @@ import io.sobok.SobokSobok.auth.domain.QUser;
 import io.sobok.SobokSobok.friend.domain.QSendFriend;
 import io.sobok.SobokSobok.notice.domain.QNotice;
 import io.sobok.SobokSobok.notice.ui.dto.NoticeInfo;
+import io.sobok.SobokSobok.notice.ui.dto.ReceivePillInfoResponse;
 import io.sobok.SobokSobok.pill.domain.QPill;
+import io.sobok.SobokSobok.pill.domain.QPillSchedule;
 import io.sobok.SobokSobok.pill.domain.QSendPill;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.util.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -48,5 +51,38 @@ public class NoticeQueryRepository {
                 .leftJoin(pill).on(sendPill.pillId.eq(pill.id))
                 .where(notice.receiverId.eq(receiverId))
                 .fetch();
+    }
+
+    public ReceivePillInfoResponse getReceivePillInfo(Long noticeId, Long pillId) {
+        QNotice notice = QNotice.notice;
+        QSendPill sendPill = QSendPill.sendPill;
+        QPill pill = QPill.pill;
+        QPillSchedule pillSchedule = QPillSchedule.pillSchedule;
+
+        List<Tuple> result = queryFactory
+                .selectDistinct(pill.pillName, pill.startDate, pill.endDate, pill.scheduleDay, pillSchedule.scheduleTime)
+                .from(notice)
+                .join(sendPill).on(notice.id.eq(sendPill.noticeId))
+                .join(pill).on(sendPill.pillId.eq(pill.id))
+                .join(pillSchedule).on(pill.id.eq(pillSchedule.pillId))
+                .where(
+                        notice.id.eq(noticeId),
+                        pill.id.eq(pillId)
+                )
+                .fetch();
+
+        List<String> scheduleTimeList = new ArrayList<>(result.stream()
+                .map(data -> data.get(4, String.class)).distinct().toList());
+        Collections.sort(scheduleTimeList);
+
+        Tuple firstDate = result.get(0);
+
+        return ReceivePillInfoResponse.builder()
+                .pillName(firstDate.get(0, String.class))
+                .scheduleTime(scheduleTimeList)
+                .startDate(firstDate.get(1, LocalDate.class))
+                .endDate(firstDate.get(2, LocalDate.class))
+                .scheduleDay(firstDate.get(3, String.class))
+                .build();
     }
 }
