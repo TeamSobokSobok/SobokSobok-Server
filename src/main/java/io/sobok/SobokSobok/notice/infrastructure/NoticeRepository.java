4a0485736254f9328ea84dd68e4d2b873d4ac83b
dev-Crayon <seungheon328@gmail.com>
2024-01-31 18:28:32 +0900

    [Feat]: 친구 요청 여부 확인 API
    관련 로직 작성
    
    Related to: #70

--- src/main/java/io/sobok/SobokSobok/notice/infrastructure/NoticeRepository.java
+++ src/main/java/io/sobok/SobokSobok/notice/infrastructure/NoticeRepository.java
@@ -1,9 +1,14 @@
 package io.sobok.SobokSobok.notice.infrastructure;
 
 import io.sobok.SobokSobok.notice.domain.Notice;
+import io.sobok.SobokSobok.notice.domain.NoticeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+    // READ
+    Boolean existsBySenderIdAndReceiverIdAndIsOkay(Long senderId, Long receiverId, NoticeStatus isOkay);
 }
