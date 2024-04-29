4f30dc298454507c45c63f5a1db2859d25182167
dev-Crayon <seungheon328@gmail.com>
2024-04-29 11:42:30 +0900

    [Feat]: 유저 기기 플랫폼 구분 및 푸시알림
    유저 기기 플래폼 구분 Platform 클래스 추가
    Android 푸시알림 분기처리
    
    Related to: #126

--- src/main/java/io/sobok/SobokSobok/external/firebase/FCMPushService.java
+++ src/main/java/io/sobok/SobokSobok/external/firebase/FCMPushService.java
@@ -5,6 +5,7 @@ import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import io.sobok.SobokSobok.auth.application.util.UserServiceUtil;
+import io.sobok.SobokSobok.auth.domain.Platform;
 import io.sobok.SobokSobok.auth.domain.User;
 import io.sobok.SobokSobok.auth.infrastructure.UserRepository;
 import io.sobok.SobokSobok.external.firebase.dto.PushNotificationRequest;
@@ -22,9 +23,15 @@ public class FCMPushService {
     private final UserRepository userRepository;
 
     public void sendNotificationByToken(PushNotificationRequest request) {
-
         User user = UserServiceUtil.findUserById(userRepository, request.userId());
+        if (user.getPlatform() == Platform.iOS) {
+            sendNotification(user, request);
+        } else {
+            sendDataMessage(user, request);
+        }
+    }
 
+    private void sendNotification(User user, PushNotificationRequest request) {
         Notification notification = Notification.builder()
                 .setTitle(request.title())
                 .setBody(request.body())
@@ -35,10 +42,24 @@ public class FCMPushService {
                 .setNotification(notification)
                 .build();
 
+        sendMessageToFirebase(message, request.userId());
+    }
+
+    private void sendDataMessage(User user, PushNotificationRequest request) {
+        Message message = Message.builder()
+                .putData("title", request.title())
+                .putData("body", request.body())
+                .setToken(user.getDeviceToken())
+                .build();
+
+        sendMessageToFirebase(message, request.userId());
+    }
+
+    private void sendMessageToFirebase(Message message, Long userId) {
         try {
             firebaseMessaging.send(message);
         } catch (FirebaseMessagingException e) {
-            log.error("푸시알림 전송에 실패했습니다. userId: " + request.userId() + "\n" + e.getMessage());
+            log.error("Failed to send push notification. userId: {}\n{}", userId, e.getMessage());
         }
     }
 }
